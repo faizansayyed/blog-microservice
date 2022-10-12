5e2881723ed684723807ba37c28d4ca7e84846cf
Faizan Sayyed <faizansayyed36@gmail.com>
2022-10-12 22:27:58 +0530

    initial setup with naive approach

---
+++ comments/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    return res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({
+        id: commentId,
+        content,
+        status: 'pending',
+    });
+
+    commentsByPostId[req.params.id] = comments;
+
+
+    return res.status(201).send(comments);
+});
+
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
