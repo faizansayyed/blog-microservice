db418edd0b20dca1042d183596081255ff505815
Faizan Sayyed <faizansayyed36@gmail.com>
2022-10-17 14:56:28 +0530

    created client dpl, added ingress and updated api end point for all sections

--- posts/index.js
+++ posts/index.js
@@ -11,35 +11,35 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    return res.send(posts);
+  return res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    posts[id] = {
-        id,
-        title,
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'PostCreated',
-        data: {
-            id,
-            title,
-        },
-    });
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
 
-    return res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
-    console.log('Event Received', req.body.type);
+  console.log('Received Event', req.body.type);
 
-    return res.send({});
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('Listening on 4000');
+  console.log('Listening on 4000');
 });
